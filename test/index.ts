b7584ffe5adaba720dd4d08a151314ea7e4ef38e
James Forbes <james.a.forbes@gmail.com>
2024-02-14 09:34:49 +0000

    Make default test pass show that CI works

--- test/index.ts
+++ test/index.ts
@@ -2,5 +2,5 @@ import test from 'node:test'
 import assert from 'node:assert'
 
 test('Example', () => {
-    assert.equal(false, true, 'Fix this test')
+    assert.equal(true, true, 'Fix this test')
 })
